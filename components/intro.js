e36df7b03540f8d551608cbb7fe87c4728ecfbe9
duartecancela <duartecancela@gmail.com>
2020-07-02 13:59:42 +0100

    remove alert

--- components/intro.js
+++ components/intro.js
@@ -8,14 +8,7 @@ export default function Intro() {
        <Logo /> <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         { SITE_NAME } </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
+      Web Development, Audio And Computer Science.
       </h4>
     </section>
   )
