bf1faea6b62dddf98dce3b7bb8628f89cd48158b
duartecancela <duartecancela@gmail.com>
2020-07-02 13:30:54 +0100

    first commit

---
+++ components/logo.js
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <div className="flex items-center">
+      <img src="/favicon/digitaldeveloper_logo.svg" className="pr-4 mt-6" alt="" />
+    </div>
+  )
+}
